cf79eff62fce6c39f5c28b7d7718a4566baaca1e
root <root@localhost.localdomain>
2016-07-21 13:01:45 +0530

    cookbooks commited

---
+++ reshma/cookbooks/myfirstcookbook/metadata.rb
@@ -0,0 +1,7 @@
+name 'myfirstcookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures myfirstcookbook'
+long_description 'Installs/Configures myfirstcookbook'
+version '0.1.0'
