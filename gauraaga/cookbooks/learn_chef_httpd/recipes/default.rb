e81ef64c25de5d1a05f7f8613914fa8d60dca985
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2016-07-21 14:16:20 +0530

    Added template and index.html

--- gauraaga/cookbooks/learn_chef_httpd/recipes/default.rb
+++ gauraaga/cookbooks/learn_chef_httpd/recipes/default.rb
@@ -1,35 +1,11 @@
-file 'test.txt'
-
-file 'test1.txt' do
-	owner 'trainee'
-	group 'users'
+package 'httpd'
+service 'httpd' do
+	action[:start,:enable]
 end
-
-file 'test1.txt' do
-	content "This is my first chef program"
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
-
-directory '/etc/dir1'
-directory '/root/dir2'
-directory 'dir3' do
-  owner 'trainee'
-  action :create
-end
-
-script 'updateyum' do
-  interpreter "bash"
-  code <<-EOH
-	mkdir -p /etc/apache
-	cd /etc/apache
-	wget http://mirror.fibergrid.in/apache//httpd/binaries/netware/httpd_2.4.10-netware-bin.zip
-	unzip httpd_2.4.10-netware-bin.zip
-  EOH
-end
-yum_package 'httpd' do
-	action :upgrade
-end
-
-service "httpd" do
-	action [ :stop, :disable ]
+service 'iptables' do
+	action :stop
 end
 
