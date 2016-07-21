a0331452f269c02f312eb9a11652ec53823b57fc
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2016-07-21 14:22:42 +0530

    Applied template

--- gauraaga/cookbooks/learn_chef_httpd/recipes/default.rb
+++ gauraaga/cookbooks/learn_chef_httpd/recipes/default.rb
@@ -1,11 +1,11 @@
-package 'httpd'
-service 'httpd' do
-	action[:start,:enable]
-end
+#package 'httpd'
+#service 'httpd' do
+#	action[:start,:enable]
+#end
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
 end
-service 'iptables' do
-	action :stop
-end
-
+#service 'iptables' do
+#	action :stop
+#end
+#
