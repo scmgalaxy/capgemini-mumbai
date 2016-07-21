57f5d19cb405c9ff141245798c891396d569bf57
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2016-07-21 12:44:34 +0530

    Added custom cookbook

---
+++ gauraaga/custom_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name 'custom_cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures custom_cookbook'
+long_description 'Installs/Configures custom_cookbook'
+version '0.1.0'
